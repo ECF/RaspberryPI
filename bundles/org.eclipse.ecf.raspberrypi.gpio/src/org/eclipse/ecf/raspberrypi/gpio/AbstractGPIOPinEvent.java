71a59e24c49592964652f49688ef2a5089fc9f86
slewis <slewis@composent.com>
2014-07-31 11:07:58 -0700

    Added IGPIOPinInputListener and supporting event classes for digital pin event change handling.

---
+++ bundles/org.eclipse.ecf.raspberrypi.gpio/src/org/eclipse/ecf/raspberrypi/gpio/AbstractGPIOPinEvent.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Composent, Inc. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Scott Lewis - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ecf.raspberrypi.gpio;
+
+import java.io.Serializable;
+
+public abstract class AbstractGPIOPinEvent implements Serializable {
+	private static final long serialVersionUID = 6733232691622858769L;
+
+	private final int pinId;
+	
+	public AbstractGPIOPinEvent(int pinId) {
+		this.pinId = pinId;
+	}
+	
+	public int getPinId() {
+		return pinId;
+	}
+}
---
+++ bundles/org.eclipse.ecf.raspberrypi.gpio/src/org/eclipse/ecf/raspberrypi/gpio/GPIOPinInputEvent.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Composent, Inc. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Scott Lewis - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ecf.raspberrypi.gpio;
+
+import java.io.Serializable;
+
+public class GPIOPinInputEvent extends AbstractGPIOPinEvent implements Serializable {
+
+	private static final long serialVersionUID = -6103636181685626657L;
+
+	private final boolean state;
+	
+	public GPIOPinInputEvent(int pinId, boolean state) {
+		super(pinId);
+		this.state = state;
+	}
+
+	public boolean getState() {
+		return state;
+	}
+}
