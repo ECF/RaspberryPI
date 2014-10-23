968bb7f17ebbe276a1d16db895bc8c2d88f1dea0
Wim Jongman <wim.jongman@remainsoftware.com>
2014-10-23 14:43:58 +0200

    Putting each pin in own bundle refactoring duplicate code Moving unregistration in own thread because of JmDNS lock

--- test/bundles/org.eclipse.ecf.raspberrypi.gpio.pin1/src/org/eclipse/ecf/raspberrypi/gpio/pin1/Activator.java
+++ test/bundles/org.eclipse.ecf.raspberrypi.gpio.pin1/src/org/eclipse/ecf/raspberrypi/gpio/pin1/Activator.java
@@ -8,121 +8,27 @@
  ******************************************************************************/
 package org.eclipse.ecf.raspberrypi.gpio.pin1;
 
-import java.net.InetAddress;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
-import org.eclipse.ecf.raspberrypi.gpio.GPIOPinInputEvent;
-import org.eclipse.ecf.raspberrypi.gpio.IGPIOPinInputListener;
-import org.eclipse.ecf.raspberrypi.gpio.IGPIOPinOutput;
 import org.eclipse.ecf.raspberrypi.gpio.pi4j.Pi4jGPIOPinOutput;
-import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.framework.ServiceRegistration;
-import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
-public class Activator implements BundleActivator {
+public class Activator extends org.eclipse.ecf.raspberrypi.gpio.pin0.Activator {
 
 	private static BundleContext context;
 
 	static BundleContext getContext() {
 		return context;
 	}
-
-	private ServiceTracker<IGPIOPinOutput, IGPIOPinOutput> pinTracker;
-	private ServiceRegistration<IGPIOPinOutput> regPin1;
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
-	 * )
-	 */
+	
+	@Override
 	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-		// Setup properties for export using the ecf generic server
-		Map<String, Object> pinProps = new HashMap<String, Object>();
-		pinProps.put("service.exported.interfaces", "*");
-		pinProps.put("service.exported.configs", "ecf.generic.server");
-		pinProps.put("ecf.generic.server.port", "3288");
-		pinProps.put("ecf.generic.server.hostname",InetAddress.getLocalHost().getHostAddress());
-		pinProps.put("ecf.exported.async.interfaces", "*");
-		Properties systemProps = System.getProperties();
-		for(Object pn: systemProps.keySet()) {
-			String propName = (String) pn;
-			if (propName.startsWith("service.") || propName.startsWith("ecf.")) 
-				pinProps.put(propName,systemProps.get(propName));
-		}
+		super.start(bundleContext);
+		
+	}
 
+	public void registerPin(Map<String, Object> pinProps) {
 		// register GPIOPin 0 with the above export properties
-		regPin1 = Pi4jGPIOPinOutput.registerGPIOPinOutput(1, pinProps, context);
-
-		// Create tracker to print out information from registration above
-		pinTracker = new ServiceTracker<IGPIOPinOutput, IGPIOPinOutput>(
-				context, IGPIOPinOutput.class,
-				new ServiceTrackerCustomizer<IGPIOPinOutput, IGPIOPinOutput>() {
-
-					@Override
-					public IGPIOPinOutput addingService(
-							ServiceReference<IGPIOPinOutput> reference) {
-						System.out.println("Adding GPIO Pin Output service.   id="
-								+ reference
-										.getProperty(IGPIOPinOutput.PIN_ID_PROP));
-						IGPIOPinOutput pin = context.getService(reference);
-						System.out.println("  current pin state is "
-								+ (pin.getState() ? "HIGH" : "LOW"));
-						System.out.println("  setting state to HIGH");
-						pin.setState(true);
-						return pin;
-					}
-
-					@Override
-					public void modifiedService(
-							ServiceReference<IGPIOPinOutput> reference,
-							IGPIOPinOutput service) {
-					}
-
-					@Override
-					public void removedService(
-							ServiceReference<IGPIOPinOutput> reference,
-							IGPIOPinOutput pin) {
-						System.out.println("Removing GPIO Pin service. id="
-								+ reference
-										.getProperty(IGPIOPinOutput.PIN_ID_PROP));
-						System.out.println("  setting state to LOW");
-						pin.setState(false);
-					}
-				});
-		pinTracker.open();
-	}
-
-	class TestGPIOPinInputListener implements IGPIOPinInputListener {
-		@Override
-		public void handleInputEvent(GPIOPinInputEvent event) {
-			System.out
-					.println("TestGPIOPinInputListener.handleInputEvent(event="
-							+ event + ")");
-		}
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		if (pinTracker != null) {
-			pinTracker.close();
-			pinTracker = null;
-		}
-		if (regPin1 != null) {
-			regPin1.unregister();
-			regPin1 = null;
-		}
+		setPin(Pi4jGPIOPinOutput.registerGPIOPinOutput(1, pinProps, getContext()));
 	}
 }
